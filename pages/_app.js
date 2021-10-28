7c8de6ef9386d8e60814c4854ce6952cb5599f12
bilord <igor.buksheff@yandex.ru>
2021-10-28 04:12:01 +0300

    fix: in _app

--- pages/_app.js
+++ pages/_app.js
@@ -31,7 +31,7 @@ const MyApp = (observer((props) => {
 
   const router = useRouter()
 
-  useEffect(() => {
+  React.useEffect(() => {
     initGA()
     // `routeChangeComplete` won't run for the first page load unless the query string is
     // hydrated later on, so here we log a page view if this is the first render and
@@ -42,7 +42,7 @@ const MyApp = (observer((props) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
-  useEffect(() => {
+  React.useEffect(() => {
     // Listen for page changes after a navigation or when the query changes
     router.events.on('routeChangeComplete', logPageView)
     return () => {
