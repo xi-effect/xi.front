08e2b2094115d5d730e4d0106e69661f3c4f2e63
bilordigor <igor.buksheff@yandex.ru>
2021-11-11 20:55:13 +0300

    feat (modules): some fix

--- pages/index.js
+++ pages/index.js
@@ -1,110 +1,119 @@
-import Head from 'next/head'
-import { styled } from '@mui/material/styles';
-import Router from 'next/router'
-import Image from 'next/image'
-import React from 'react';
-import clsx from 'clsx'
-import { useRouter } from 'next/router'
+import ArrowDownwardIcon from "@mui/icons-material/ArrowDownward";
+import {
+  Box,
+  Button,
+  Divider,
+  Grid,
+  IconButton,
+  Link,
+  Paper,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
+import { styled } from "@mui/material/styles";
+import clsx from "clsx";
+import { inject, observer } from "mobx-react";
+import Head from "next/head";
+import Image from "next/image";
+import Router, { useRouter } from "next/router";
+import React from "react";
+import Loading from "./../components/OtherComponents/Loading/Loading";
+import CardsList from "./../components/PagesComponents/Landing/CardList";
+import Header from "./../components/PagesComponents/Landing/Header";
 
-import { Divider, useMediaQuery, Link, Button, IconButton, Grid, Box, Paper, useTheme, Typography } from '@mui/material';
+const Main = inject(
+  "rootStore",
+  "uiStore"
+)(
+  observer(({ rootStore, uiStore }) => {
+    const theme = useTheme();
+    const [loading, setLoading] = React.useState(true);
+    const router = useRouter();
+    const mobile = useMediaQuery((theme) => theme.breakpoints.down("xl"));
 
+    const firstCard = React.useRef(null);
 
-import { inject, observer } from 'mobx-react'
-import Loading from './../components/OtherComponents/Loading/Loading';
-import Header from './../components/PagesComponents/Landing/Header';
-import CardsList from './../components/PagesComponents/Landing/CardList';
+    const executeScroll = () =>
+      firstCard.current.scrollIntoView({ behavior: "smooth" });
 
-import ArrowDownwardIcon from '@mui/icons-material/ArrowDownward';
-
-
-const Main = inject('rootStore', 'uiStore')(observer(({ rootStore, uiStore }) => {
-  const theme = useTheme();
-  const [loading, setLoading] = React.useState(true)
-  const router = useRouter()
-  const mobile = useMediaQuery(theme => theme.breakpoints.down('xl'));
-
-  const firstCard = React.useRef(null)
-
-  const executeScroll = () => firstCard.current.scrollIntoView({ behavior: "smooth" })
-
-  return (
-    <>
-      <Head>
-        <title>
-          Ξffect
-        </title>
-      </Head>
-      {/* <Background /> */}
-      {loading && <Loading />}
-      <Grid
-        container
-        direction="column"
-        justifyContent="flex-start"
-        alignItems="center"
-        sx={{
-          width: '100%',
-          height: '100%',
-        }}
-      >
-        <Header />
+    return (
+      <>
+        <Head>
+          <title>Ξffect</title>
+        </Head>
+        {/* <Background /> */}
+        {loading && <Loading />}
         <Grid
-          item
-          container
-          direction="row"
-          justifyContent="center"
-          alignItems="center"
-          sx={{
-            position: "relative",
-            backgroundColor: 'background.1',
-            width: "100%",
-            paddingTop: "41.8%",
-            background: `linear-gradient(0deg, ${theme.palette.constant.landingPink} , ${theme.palette.constant.landingBlue})`,
-          }}>
-          <Image
-            alt="alt"
-            src={"/backgroundImg.jpg"}
-            layout="fill"
-            objectFit="cover"
-            quality={100}
-            onLoadingComplete
-            ={() =>
-              setLoading(false)
-            }
-          />
-        </Grid>
-        <Grid
-          item
           container
           direction="column"
-          justifyContent="center"
+          justifyContent="flex-start"
           alignItems="center"
           sx={{
-            width: '100%',
-            height: "196px",
-            background: `linear-gradient(0deg, ${theme.palette.background["2"]} , ${theme.palette.constant.landingPink})`,
+            width: "100%",
+            height: "100%",
           }}
         >
+          <Header />
+          <Grid
+            item
+            container
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+            sx={{
+              position: "relative",
+              backgroundColor: "background.1",
+              width: "100%",
+              paddingTop: "41.8%",
+              background: `linear-gradient(0deg, ${theme.palette.constant.landingPink} , ${theme.palette.constant.landingBlue})`,
+            }}
+          >
+            <Image
+              alt="alt"
+              src={"/backgroundImg.jpg"}
+              layout="fill"
+              objectFit="cover"
+              quality={100}
+              onLoadingComplete={() => setLoading(false)}
+            />
+          </Grid>
           <Grid
             item
             container
             direction="column"
-            justifyContent="flex-start"
+            justifyContent="center"
             alignItems="center"
-            sx={{ maxWidth: 1200, }}
+            sx={{
+              width: "100%",
+              height: "196px",
+              background: `linear-gradient(0deg, ${theme.palette.background["2"]} , ${theme.palette.constant.landingPink})`,
+            }}
           >
-            <IconButton onClick={executeScroll} size="large">
-              <ArrowDownwardIcon sx={{
-                fontSize: 48,
-                color: 'constant.textWhite',
-              }} />
-            </IconButton>
+            <Grid
+              item
+              container
+              direction="column"
+              justifyContent="flex-start"
+              alignItems="center"
+              sx={{ maxWidth: 1200 }}
+            >
+              <IconButton onClick={executeScroll} size="large">
+                <ArrowDownwardIcon
+                  sx={{
+                    fontSize: 48,
+                    color: "constant.textWhite",
+                  }}
+                />
+              </IconButton>
+            </Grid>
           </Grid>
+          <div ref={firstCard}> </div>
+          <CardsList />
         </Grid>
-        <div ref={firstCard}> </div>
-        <CardsList />
-      </Grid>
-    </>
-  );
-}))
+      </>
+    );
+  })
+);
 
-export default Main
+export default Main;
