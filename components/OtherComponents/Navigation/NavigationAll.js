f43be2dadc61c014e34ba579e24ddd860fa8ef2c
bilord <igor.buksheff@yandex.ru>
2021-12-04 03:58:15 +0300

    feat (Navigation): start work on mobile menu

--- components/OtherComponents/Navigation/NavigationAll.js
+++ components/OtherComponents/Navigation/NavigationAll.js
@@ -19,6 +19,7 @@ import { io } from "socket.io-client";
 import socket from "../../../utils/socket";
 
 import { Scrollbars } from 'react-custom-scrollbars-2';
+import { motion, useMotionValue, useTransform, useDragControls } from "framer-motion"
 
 const NavigationAll = inject(
   "rootStore",
@@ -30,7 +31,7 @@ const NavigationAll = inject(
     const theme = useTheme();
     const router = useRouter();
 
-    const mobile = useMediaQuery((theme) => theme.breakpoints.down("md"));
+    const mobile = useMediaQuery((theme) => theme.breakpoints.down("sm"));
 
     React.useEffect(() => {
       // Главное подключение к сокету
@@ -88,89 +89,204 @@ const NavigationAll = inject(
       if (haveRightToolbar) w = w + 48
       if (haveRightMenu) w = w + 156
       if (hoverLeftName !== null) w = w + 156
-      if (mobile) w = 32
       return w
     }
 
     const getBorderTopRightRadius = () => {
       let btrr = (haveRightMenu || haveRightToolbar) ? 32 : 2
-      if (mobile) btrr = 24
       return btrr
     }
 
     const getMarginLeft = () => {
       let ml = "70px"
-      if (mobile) ml = 2
       if (hoverLeftName !== null) ml = "226px"
       return ml
     }
 
+    const getWidthMobile = () => {
+      let w = 32
+      return w
+    }
 
-    return (
-      <>
-        {uiStore.loading["navigation"] && <Loading />}
-        {!uiStore.loading["navigation"] && (
-          <Box
-            sx={{
-              zIndex: 0,
-              // display: "flex",
-              backgroundColor: "primary.main",
-              minHeight: "100vh",
-              overflow: 'hidden',
-              // width: "calc(100% + 16px)",
-            }}
-          >
-            <Upbar haveRightMenu={haveRightMenu} haveRightToolbar={haveRightToolbar} />
-            <Sidebar hoverLeftName={hoverLeftName} setHoverLeftName={setHoverLeftName} />
-            <SidebarSecond hoverLeftName={hoverLeftName} />
-            {haveRightToolbar && <RightToolbar />}
-            {haveRightMenu && <RightMenu />}
-            {/* <Box
+    const x = useMotionValue(0)
+    const opacity = useTransform(x, [-100, 0, 100], [0.4, 1, 0.4])
+
+    const [dragX, setDragX] = React.useState('center')
+
+    // const dragControls = useDragControls()
+
+    // function startDrag(event) {
+    //   dragControls.start(event, { snapToCursor: true })
+    // }
+
+    if (!mobile) {
+      return (
+        <>
+          {uiStore.loading["navigation"] && <Loading />}
+          {!uiStore.loading["navigation"] && (
+            <Box
               sx={{
-                display: {
-                  xs: "block",
-                  sm: "block",
-                  md: "none",
-                  lg: "none",
-                  xl: "none",
-                },
+                zIndex: 0,
+                // display: "flex",
+                backgroundColor: "primary.main",
+                minHeight: "100vh",
+                overflow: 'hidden',
+                // width: "calc(100% + 16px)",
               }}
             >
-              <SideDownbar />
-            </Box> */}
-            <Paper
-              onMouseEnter={() => setHoverLeftName(null)}
-              elevation={2}
-              sx={{
-                transition: '0.8s',
-                zIndex: 1,
-                margin: 0,
-                overflow: 'auto',
-                width: `calc(100% - ${getWidth()}px)`,
-                height: "calc(100vh - 48px)",
-                marginLeft: getMarginLeft(),
-                borderTopLeftRadius: mobile ? 24 : 32,
-                borderTopRightRadius: getBorderTopRightRadius(),
-                backgroundColor: "background.main",
-
-              }}
-            >
-              {!(router.pathname.includes('/message')) && <Scrollbars
-                universal={true}
-                style={{ width: "100%", height: "100%" }}
-                autoHide
-                autoHideTimeout={1000}
-                autoHideDuration={200}
+              <Upbar haveRightMenu={haveRightMenu} haveRightToolbar={haveRightToolbar} />
+              <Sidebar hoverLeftName={hoverLeftName} setHoverLeftName={setHoverLeftName} />
+              <SidebarSecond hoverLeftName={hoverLeftName} />
+              {haveRightToolbar && <RightToolbar />}
+              {haveRightMenu && <RightMenu />}
+              {/* <Box
+                sx={{
+                  display: {
+                    xs: "block",
+                    sm: "block",
+                    md: "none",
+                    lg: "none",
+                    xl: "none",
+                  },
+                }}
               >
-                {children}
-              </Scrollbars>}
-              {router.pathname.includes('/message') && children}
-            </Paper>
-            {/* <ChatDialog /> */}
-          </Box>
-        )}
-      </>
-    );
+                <SideDownbar />
+              </Box> */}
+              <Paper
+                onMouseEnter={() => setHoverLeftName(null)}
+                elevation={2}
+                sx={{
+                  transition: '0.8s',
+                  zIndex: 1,
+                  margin: 0,
+                  overflow: 'auto',
+                  width: `calc(100% - ${getWidth()}px)`,
+                  height: "calc(100vh - 48px)",
+                  marginLeft: getMarginLeft(),
+                  borderTopLeftRadius: 32,
+                  borderTopRightRadius: getBorderTopRightRadius(),
+                  backgroundColor: "background.main",
+
+                }}
+              >
+                {!(router.pathname.includes('/message')) && <Scrollbars
+                  universal={true}
+                  style={{ width: "100%", height: "100%" }}
+                  autoHide
+                  autoHideTimeout={1000}
+                  autoHideDuration={200}
+                >
+                  {children}
+                </Scrollbars>}
+                {router.pathname.includes('/message') && children}
+              </Paper>
+              {/* <ChatDialog /> */}
+            </Box>
+          )}
+        </>
+      );
+    }
+
+    const dragVariants = {
+      left: {
+        x: "200px",
+      },
+      center: {
+        x: 0,
+      },
+      right: {
+        x: "-200px",
+      }
+    }
+
+    if (mobile) {
+      return (
+        <>
+          {uiStore.loading["navigation"] && <Loading />}
+          {!uiStore.loading["navigation"] && (
+            <Box
+              sx={{
+                zIndex: 0,
+                // display: "flex",
+                backgroundColor: "primary.main",
+                minHeight: "100vh",
+                overflow: 'hidden',
+                // width: "calc(100% + 16px)",
+              }}
+            >
+              <Upbar haveRightMenu={haveRightMenu} haveRightToolbar={haveRightToolbar} />
+              {/* <Sidebar hoverLeftName={hoverLeftName} setHoverLeftName={setHoverLeftName} /> */}
+              {/* <SidebarSecond hoverLeftName={hoverLeftName} /> */}
+              {/* {haveRightToolbar && <RightToolbar />} */}
+              {/* {haveRightMenu && <RightMenu />} */}
+              {/* <Box
+                sx={{
+                  display: {
+                    xs: "block",
+                    sm: "block",
+                    md: "none",
+                    lg: "none",
+                    xl: "none",
+                  },
+                }}
+              >
+                <SideDownbar />
+              </Box> */}
+              <Paper
+                onMouseEnter={() => setHoverLeftName(null)}
+                elevation={2}
+                sx={{
+                  transition: '0.8s',
+                  zIndex: 1,
+                  margin: 0,
+                  overflow: 'auto',
+                  width: `calc(100% - ${getWidthMobile()}px)`,
+                  // width: `100%`,
+                  height: "calc(100vh - 48px)",
+                  // marginLeft: getMarginLeftMobile(),
+                  ml: 2,
+                  // mr: 1,
+                  borderTopLeftRadius: 24,
+                  borderTopRightRadius: 24,
+                  backgroundColor: "background.main",
+                }}
+                drag
+                dragDirectionLock
+                onDirectionLock={axis => console.log(axis)}
+                style={{ opacity }}
+                // dragConstraints={{ left: 100, right: 100 }}
+                variants={dragVariants}
+                animate={() => {
+                  if (dragX === "left") return "left"
+                  if (dragX === "center") return "center"
+                  if (dragX === "right") return "right"
+                }}
+                component={motion.div}
+                // onPan={onPan}
+                dragElastic={0.8}
+                onDragStart={
+                  (event, info) => console.log("onDragStart", info, info.point.x, info.point.y)
+                }
+              >
+                {!(router.pathname.includes('/message')) && <Scrollbars
+                  universal={true}
+                  style={{ width: "100%", height: "100%" }}
+                  autoHide
+                  autoHideTimeout={1000}
+                  autoHideDuration={200}
+                >
+                  {children}
+
+                </Scrollbars>}
+                {router.pathname.includes('/message') && children}
+              </Paper>
+              {/* <ChatDialog /> */}
+            </Box>
+          )}
+        </>
+      );
+    }
+
   })
 );
 
