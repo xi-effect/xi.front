0f724ba7d7b38a64008e4821b051cd2dfe910460
Евгений Жиров <girov189916@gmail.com>
2021-12-27 22:38:34 +0300

    new design email form

--- pages/resetpassword/email.js
+++ pages/resetpassword/email.js
@@ -2,7 +2,10 @@ import Head from 'next/head'
 import { useRouter } from 'next/router'
 import Image from "next/image";
 import clsx from 'clsx';
-import { Grid, Stack, Input, Link, useMediaQuery, TextField, useTheme, InputLabel, InputAdornment, Tooltip, IconButton, FormControl, OutlinedInput, FormControlLabel, Switch, AppBar, Tabs, Tab, Typography, Box, Button, Paper } from '@mui/material';
+import { Grid, Stack, Input, Link, 
+    useMediaQuery, TextField, useTheme, 
+    InputLabel, InputAdornment, Tooltip, IconButton, 
+    FormControl, OutlinedInput, FormControlLabel, Switch, AppBar, Tabs, Tab, Typography, Box, Button, Paper } from '@mui/material';
 import { Link as LinkUI } from '@mui/material';
 import React from 'react'
 import BackgroundImg from '../../components/OtherComponents/Background/BackgroundImg'
@@ -25,6 +28,7 @@ const schema = yup.object({
 const PassResetEmail = inject('rootStore', 'uiStore', 'authorizationStore')(observer(({ rootStore, uiStore, authorizationStore }) => {
     const theme = useTheme();
     const router = useRouter()
+    const mobile = useMediaQuery(theme => theme.breakpoints.down('md'));
     const { control, handleSubmit, formState: { errors } } = useForm({
         resolver: yupResolver(schema)
     });
@@ -48,42 +52,85 @@ const PassResetEmail = inject('rootStore', 'uiStore', 'authorizationStore')(obse
                 sx={{
                     width: "100vw",
                     height: "100vh",
-                    backgroundColor: 'background.1',
+                    backgroundColor: 'background.main',
                 }}
             >
-                <Box
-                    sx={{
-                    position: "fixed",
-                    height: "100vh",
-                    width: "100vw",
-                    overflow: "hidden",
-                    zIndex: "-1",
-                    }}
-                >
-                    <Image
-                        alt="alt"
-                        src={"/svg/BackgroundWaves.svg"}
-                        layout="fill"
-                        objectFit="cover"
-                        quality={100}
-                        // onLoadingComplete={() => setLoading(false)}
-                    />
-                </Box>
                 <Stack
-                    direction="row"
-                    justifyContent="flex-start"
-                    alignItems="center"
-                    spacing={0}
-                    sx={{ width: "100%" }}
-                >
-                    <Typography
-                        onClick={() => router.push("/")}
-                        variant="h4"
-                        sx={{ color: "text.main", m: 2, zIndex: 2, cursor: "pointer" }}
-                    >
-                        Ξffect
-                    </Typography>
-                </Stack>
+            component={motion.div}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 1, duration: 1 }}
+            direction="row"
+            justifyContent="space-between"
+            alignItems="center"
+            sx={{ height: mobile ? '100px' : "140px", p: mobile ? '20px' : '40px', width: '100%', }}
+          >
+            <Stack
+              direction="row"
+              justifyContent="flex-start"
+              alignItems="baseline"
+            >
+              <Typography
+                component={"h1"}
+                onClick={() => {
+                  router.push({
+                    pathname: '/',
+                  })
+                }}
+
+                variant="Roboto500XiLabel"
+                sx={{
+                  mt: '1px',
+                  cursor: 'pointer',
+                  color: 'secondary.main',
+                  fontSize: {
+                    sm: '28px',
+                    md: '34px',
+                    lg: '40px',
+                  },
+                }}
+              >
+                Ξ
+              </Typography>
+              <Typography
+                component={"h1"}
+                onClick={() => {
+                  router.push({
+                    pathname: '/',
+                  })
+                }}
+
+                variant="IBMPlexMono500XiLabelEnd"
+                sx={{
+                  '&.MuiTypography-root': {
+                    cursor: 'pointer',
+                    color: 'secondary.main',
+                  },
+                  fontSize: {
+                    sm: '28px',
+                    md: '34px',
+                    lg: '40px',
+                  },
+                }}
+              >
+                ffect
+              </Typography>
+            </Stack>
+          </Stack>
+          <Paper
+            elevation={24}
+            sx={{
+              zIndex: 10,
+              width: 'calc(100% - 32px)',
+              maxWidth: 512,
+              // mt: mobile ? "2px" : -32,
+              mt: 0,
+              ml: mobile ? "16px" : "100px",
+              mr: mobile ? "16px" : "100px",
+              bgcolor: "grey.800",
+              borderRadius: "20px",
+            }}
+          >
                 <Box
                     component="form"
                     // bgcolor: 'rgba(1, 1, 1, 0.4)',
@@ -112,21 +159,23 @@ const PassResetEmail = inject('rootStore', 'uiStore', 'authorizationStore')(obse
                             name="email"
                             control={control}
                             defaultValue=""
-                            render={({ field }) => <FormControl error={errors?.email?.type === "required"} fullWidth sx={{ maxWidth: 512, mt: 1 }} variant="outlined">
-                                <InputLabel htmlFor="outlined-adornment-password"> <Typography sx={{ color: 'text.main' }}>Адрес почты</Typography> </InputLabel>
-                                <OutlinedInput
-                                    sx={{ backgroundColor: 'background.main', width: "100%", }}
+                            render={({ field }) => 
+                            <FormControl error={errors?.email?.type === "required"} fullWidth sx={{ maxWidth: 512, mt: 1 }} variant="outlined">
+                                <InputLabel>
+                                    <Typography sx={{ color: "text.primary" }}>
+                                    Адрес почты
+                                    </Typography>
+                                </InputLabel>
+                                <Input
+                                    sx={{ width: "100%" }}
                                     label="Адрес почты"
-                                    type='text'
-
-                                    // value={emailReset}
-                                    // onChange={null}
+                                    type="text"
                                     {...field}
                                     endAdornment={
                                         <InputAdornment position="end">
                                             <IconButton edge="end" size="large">
                                                 <Tooltip title="Ваш адресс электронной почты" arrow>
-                                                    <EmailIcon sx={{ color: 'text.main' }} />
+                                                <EmailIcon sx={{ color: "text.secondary" }} />
                                                 </Tooltip>
                                             </IconButton>
                                         </InputAdornment>
@@ -135,37 +184,54 @@ const PassResetEmail = inject('rootStore', 'uiStore', 'authorizationStore')(obse
                                 {errors?.email?.message === "email is a required field" && <Typography varinat="subtitle1" sx={{ mt: 1, ml: 1, }} color="error"> Обязательное поле </Typography>}
                                 {errors?.email?.message === "email must be a valid email" && <Typography varinat="subtitle1" sx={{ mt: 1, ml: 1, }} color="error"> Ошибка валидации </Typography>}
                                 <Link
-                                    sx={{ color: 'text.main', m: 1, cursor: 'pointer' }}
+                                    sx={{ color: 'text.secondary', m: 1, fontWeight: 500, cursor: "pointer" }}
                                     onClick={() => {
-                                        router.push({
-                                            pathname: '/login',
-                                        })
+                                    router.push({
+                                        pathname: "/resetpassword/email",
+                                    });
                                     }}
                                     underline="hover"
                                 >
                                     на страницу входа
                                 </Link>
                             </FormControl>}
+                            
                         />
                         {authorizationStore.passwordReset.errorEmailNotFounedReset && <Typography variant="subtitle1" > Пользователя с таким адресом электронной почты не существует</Typography>}
                         {!authorizationStore.passwordReset.emailResetOk &&
-                         <Button
+
+                            <Button
                             variant="outlined"
                             size="large"
                             type="submit"
                             sx={{
-                            mt: 4,
-                            color: "text.main",
-                            bgcolor: "background.main",
-                            border: `2px solid ${theme.palette.text.dark}`,
-                            "&:hover": { border: `2px solid ${theme.palette.text.dark}` },
+                            '&.MuiButton-root': {
+                                fontFamily: 'Open Sans, sans-serif',
+                                fontStyle: 'normal',
+                                fontWeight: 600,
+                                fontSize: '16px',
+                                lineHeight: '25px',
+                                width: mobile ? '220px' : '220px',
+                                height: mobile ? '40px' : '50px',
+                                color: 'text.primary',
+                                bgcolor: 'secondary.main',
+                                borderRadius: mobile ? '62px' : '88px',
+                                '&:hover': {
+                                bgcolor: 'secondary.main',
+                                },
+                                mt: 2,
+                                boxShadow: 12,
+                            }
                             }}
-                        >
+                            >
                             Отправить письмо
-                        </Button>}
+                            </Button>   
+
+                        }
                         {authorizationStore.passwordReset.emailResetOk && <Typography variant="subtitle1"> Письмо отправлено. С этой страницы можно безопасно уходить  </Typography>}
                     </Stack>
                 </Box>
+                </Paper>
                 <div>
 
                 </div>
