ed7f287413153d4ba221fd3aec89fa2b99f9642e
bilord <igor.buksheff@yandex.ru>
2022-02-02 21:21:59 +0300

    fix: imports

--- components/OtherComponents/Editor/TextEditor.tsx
+++ components/OtherComponents/Editor/TextEditor.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { Editor } from "draft-js";
 import { useEditorApi } from "./context";
 import cn from "classnames";
-import "./TextEditor.scss";
+// import "./TextEditor.scss";
 
 export type TextEditorProps = {
   className?: string;
@@ -12,7 +12,7 @@ const TextEditor: React.FC<TextEditorProps> = ({ className }) => {
   const { state, onChange } = useEditorApi();
 
   return (
-    <div className={cn("text-editor", className)}>
+    <div className={cn(className)}>
       <Editor placeholder="Введите ваш текст" editorState={state} onChange={onChange} />
     </div>
   );
