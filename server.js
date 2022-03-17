c2f4f89a0cf5cf726b0f11c7b2e071f576529fe3
bilord <igor.buksheff@yandex.ru>
2022-03-17 23:46:22 +0300

    fix: server.js

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const { createServer } = require("https");
 const { parse } = require("url");
-const { join } = require('path')
 const { readFileSync } = require("fs");
 const next = require("next");
 
@@ -18,14 +17,7 @@ app.prepare()
     .then(() => {
         createServer(httpsOptions, (req, res) => {
             const parsedUrl = parse(req.url, true);
-            const { pathname } = parsedUrl
-
-            if (pathname === '/sw.js' || /^\/(workbox|worker|fallback)-\w+\.js$/.test(pathname)) {
-                const filePath = join(__dirname, '.next', pathname)
-                app.serveStatic(req, res, filePath)
-            } else {
-                handle(req, res, parsedUrl)
-            }
+            handle(req, res, parsedUrl);
         }).listen(port, err => {
             if (err) throw err;
             console.log(`> Ready on https://localhost:${port}`);
