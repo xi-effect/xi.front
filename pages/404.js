e2f20dc1812194a3db5978cfb206b2412abd3824
Stan <s2000ProMax@gmail.com>
2022-04-13 12:24:05 +0300

    feat: add list of error messages

--- pages/404.js
+++ pages/404.js
@@ -1,6 +1,7 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
 import React from "react";
 import ErrorPage from '../components/PagesComponents/Error/ErrorPage';
+import {errorMessages} from '../texts/errorMessages/errorMessages';
 
 // eslint-disable-next-line no-underscore-dangle
 export default function _404() {
@@ -8,7 +9,7 @@ export default function _404() {
   return (
     <ErrorPage
       imageSrc="/assets/404/404Error.svg"
-      textMessage="Страница не найдена"
+      textMessage={errorMessages[404]}
     />
   );
 }
