7af9cd249abb5e3a644a37d8c77e092f86a8f9a1
bilord <igor.buksheff@yandex.ru>
2022-04-17 20:35:35 +0300

    feat: add Community Avatar generation

--- components/OtherComponents/Navigation/SidebarSecondHeaders/Community/DialogSettings/Review.js
+++ components/OtherComponents/Navigation/SidebarSecondHeaders/Community/DialogSettings/Review.js
@@ -1,12 +1,33 @@
 import React from "react";
 import { inject, observer } from "mobx-react"
 
-import { Stack, Typography, Divider } from "@mui/material";
+import { Stack, Box, Typography, FormControl, Select, InputLabel, MenuItem, Divider, IconButton } from "@mui/material";
 
 import { useForm, Controller } from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import * as yup from 'yup';
+import Avatar from "boring-avatars";
+import AutorenewIcon from '@mui/icons-material/Autorenew';
+import { blue, teal, green, orange, red, blueGrey, indigo } from '@mui/material/colors';
 import TextFieldCustom from "../../../../../../kit/TextFieldCustom";
+
+
+const getColor = () => {
+    const color = Math.floor(Math.random() * (7 - 1) + 1);
+    const colorIndex = Math.floor(Math.random() * (9 - 1) + 1);
+    switch (color) {
+        case 1: return blue[Number(`${colorIndex}00`)]
+        case 2: return teal[Number(`${colorIndex}00`)]
+        case 3: return green[Number(`${colorIndex}00`)]
+        case 4: return orange[Number(`${colorIndex}00`)]
+        case 5: return red[Number(`${colorIndex}00`)]
+        case 6: return blueGrey[Number(`${colorIndex}00`)]
+        case 7: return indigo[Number(`${colorIndex}00`)]
+        default: return green[`${colorIndex}00`]
+    }
+};
+
+const getNewColorsArray = (colors) => colors.map(() => getColor())
 
 const schema = yup
     .object({
@@ -30,6 +51,19 @@ const Review = inject()(observer(() => {
     //     trigger();
     //     // authorizationSt.clickEnterButton(data, trigger);
     // };
+
+    const [avatarType, setAvatarType] = React.useState('marble');
+
+    const handleChange = (event) => {
+        setAvatarType(event.target.value);
+    };
+
+    const [colors, setColors] = React.useState(["#92A1C6", "#146A7C", "#F0AB3D", "#C271B4", "#C20D90"]);
+
+    const generateNewColors = () => {
+        const newColors = getNewColorsArray(colors);
+        setColors(newColors);
+    }
 
     return (
         <Stack
@@ -56,6 +90,61 @@ const Review = inject()(observer(() => {
                     width: "100%"
                 }}
             >
+                <Stack
+                    direction="row"
+                    justifyContent="flex-start"
+                    alignItems="flex-start"
+                    spacing={2}
+                    sx={{
+                        width: '100%',
+                    }}
+                >
+                    <Box>
+                        <Avatar
+                            size={128}
+                            name="Maria l"
+                            variant={avatarType}
+                            colors={colors}
+                        />
+                    </Box>
+                    <Stack
+                        direction="column"
+                        justifyContent="flex-start"
+                        alignItems="flex-start"
+                        spacing={2}
+                        sx={{
+                            pl: 1,
+                        }}
+                    >
+                        <Stack
+                            direction="row"
+                            justifyContent="flex-start"
+                            alignItems="center"
+                            spacing={2}
+                        >
+                            <Typography variant="h6">
+                                Аватар сообщества
+                            </Typography>
+                            <IconButton onClick={generateNewColors}>
+                                <AutorenewIcon />
+                            </IconButton>
+                        </Stack>
+                        <FormControl variant="filled" sx={{ m: 1, minWidth: 240 }}>
+                            <InputLabel id="demo-simple-select-filled-label">Тема Аватара</InputLabel>
+                            <Select
+                                value={avatarType}
+                                onChange={handleChange}
+                            >
+                                <MenuItem value="marble">marble</MenuItem>
+                                <MenuItem value="beam">beam</MenuItem>
+                                <MenuItem value="pixel">pixel</MenuItem>
+                                <MenuItem value="sunset">sunset</MenuItem>
+                                <MenuItem value="ring">ring</MenuItem>
+                                <MenuItem value="bauhaus">bauhaus</MenuItem>
+                            </Select>
+                        </FormControl>
+                    </Stack>
+                </Stack>
                 <Controller
                     name="name"
                     control={control}
