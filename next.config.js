02b368c8c729cfc37e17d5757950e2381d0fcab9
bilord <igor.buksheff@yandex.ru>
2022-05-02 02:35:18 +0300

    fix: change next.config

--- next.config.js
+++ next.config.js
@@ -1,15 +1,13 @@
-// const { withPlausibleProxy } = require("next-plausible")
-const withPWA = require("next-pwa");
-const runtimeCaching = require("next-pwa/cache");
+import withPWA from "next-pwa";
+import runtimeCaching from "next-pwa/cache";
 
-module.exports = withPWA({
+export default withPWA({
   experimental: { esmExternals: false },
   reactStrictMode: false,
   images: {
     domains: ["xieffect.pythonanywhere.com", "cdn.discordapp.com", "localhost:3000", "localhost:5000", "xieffect.ru:5000", "xieffect.ru", "www.youtube.com"],
   },
   pwa: {
-    disable: process.env.NODE_ENV === "production",
     dest: 'public',
     register: true,
     skipWaiting: true,
