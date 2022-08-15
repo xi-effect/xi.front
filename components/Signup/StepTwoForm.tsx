bbf4f657cfbdee57df35a0ecb375b257e812fda5
Gr1mmz <admin@kraevskiy-web.ru>
2022-08-15 19:36:59 +0800

    feat: redisign signup page

---
+++ components/Signup/StepTwoForm.tsx
@@ -0,0 +1,117 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { useState } from 'react';
+import { Box, Button, IconButton, InputAdornment, Link, Stack } from '@mui/material';
+import { Control, Controller, FieldErrors, FieldValues } from 'react-hook-form';
+import TextFieldCustom from 'kit/TextFieldCustom';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+
+interface IStepTwoForm {
+  control: Control<FieldValues, object> | undefined;
+  errors: FieldErrors;
+  prevStepHandler: () => void;
+}
+
+const StepOneForm: React.FC<IStepTwoForm> = ({ control, errors, prevStepHandler }) => {
+  const [showPassword, setShowPassword] = useState<boolean>(false);
+
+  return (
+    <>
+      <Stack spacing="16px">
+        <Controller
+          name="email"
+          control={control}
+          defaultValue=""
+          render={({ field }) => (
+            <TextFieldCustom
+              variant="outlined"
+              error={!!errors.email?.message}
+              type="email"
+              fullWidth
+              placeholder="Электронная почта"
+              helperText={errors.email?.message ? 'Введите корректный e-mail' : ''}
+              {...field}
+              sx={{
+                backgroundColor: '#fff',
+              }}
+            />
+          )}
+        />
+        <Controller
+          name="password"
+          control={control}
+          defaultValue=""
+          render={({ field }) => (
+            <TextFieldCustom
+              variant="outlined"
+              error={!!errors.password?.message}
+              type={showPassword ? 'text' : 'password'}
+              fullWidth
+              placeholder="Придумайте пароль"
+              helperText={errors.password?.message ? 'Длина пароля от 6 до 100 символов' : ''}
+              {...field}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment sx={{ mr: 0.5 }} position="end">
+                    <IconButton
+                      aria-label="toggle password visibility"
+                      onClick={() => setShowPassword(!showPassword)}
+                      edge="end"
+                      size="large"
+                    >
+                      {!showPassword ? (
+                        <Visibility sx={{ color: 'gray.100' }} />
+                      ) : (
+                        <VisibilityOff sx={{ color: 'gray.100' }} />
+                      )}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+              sx={{
+                backgroundColor: '#fff',
+              }}
+            />
+          )}
+        />
+      </Stack>
+      <Box
+        sx={{
+          display: 'flex',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+        }}
+      >
+        <Link
+          underline="hover"
+          sx={{
+            cursor: 'pointer',
+            color: 'primary.dark',
+            fontWeight: 500,
+            fontSize: '16px',
+            lineHeight: '20px',
+          }}
+          onClick={() => prevStepHandler()}
+        >
+          Назад
+        </Link>
+        <Button
+          variant="contained"
+          type="submit"
+          sx={{
+            width: '120px',
+            height: '48px',
+            fontWeight: 500,
+            fontSize: '18px',
+            lineHeight: '22px',
+            textTransform: 'none',
+          }}
+        >
+          Далее
+        </Button>
+      </Box>
+    </>
+  );
+};
+
+export default StepOneForm;
--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -1,73 +1,83 @@
 /* eslint-disable jsx-a11y/anchor-is-valid */
-import Head from 'next/head';
-import { Box, Typography } from '@mui/material';
 import React from 'react';
+import Head from 'next/head';
+import { useRouter } from 'next/router';
 import { inject, observer } from 'mobx-react';
+import { Box, Link, Typography, useMediaQuery } from '@mui/material';
 import SignupForm from 'components/Signup/SignupForm';
 
 const Signup = inject()(
-  observer(() => (
-    <>
-      <Head>
-        <title>Ξffect | Регистрация</title>
-      </Head>
-      <Box
-        sx={{
-          width: '100%',
-          minHeight: '100vh',
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}
-      >
+  observer(() => {
+    const router = useRouter();
+
+    const isMobile = useMediaQuery('(max-width: 472px)');
+
+    return (
+      <>
+        <Head>
+          <title>Ξffect | Регистрация</title>
+        </Head>
         <Box
           sx={{
-            border: '1px solid #E6E6E6',
-            borderRadius: '16px',
-            padding: '38px 32px 32px 32px',
-            height: '514px',
-            width: '420px',
+            width: '100%',
+            minHeight: '100vh',
+            display: 'flex',
+            alignItems: `${isMobile ? 'flex-start' : 'center'}`,
+            justifyContent: 'center',
           }}
         >
           <Box
             sx={{
-              display: 'flex',
-              flexDirection: 'column',
-              height: '100%',
+              border: `${isMobile ? 'none' : '1px solid #E6E6E6'}`,
+              borderRadius: '16px',
+              padding: '0px 32px 32px 32px',
+              paddingTop: `${isMobile ? '65px' : '38px'}`,
+              height: '514px',
+              width: `${isMobile ? '100%' : '420px'}`,
             }}
           >
-            <Box>
-              <Box
-                sx={{
-                  margin: '0 auto',
-                  // TODO: remove 3 rows when logo is ready
-                  width: '90px',
-                  height: '13px',
-                  fontSize: '11px',
-                }}
-              >
-                logo placeholder
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'column',
+                height: '100%',
+              }}
+            >
+              <Box>
+                <Link onClick={() => router.push('/')} sx={{ cursor: 'pointer' }}>
+                  <Box
+                    sx={{
+                      margin: '0 auto',
+                      // TODO: remove 3 rows and add logo when is ready
+                      width: '90px',
+                      height: '13px',
+                      fontSize: '11px',
+                    }}
+                  >
+                    logo placeholder
+                  </Box>
+                </Link>
+                <Typography
+                  component="h1"
+                  variant="h5"
+                  marginTop="21px"
+                  sx={{
+                    textAlign: 'center',
+                    fontSize: '24px',
+                    lineHeight: '32px',
+                    fontWeight: '600',
+                  }}
+                >
+                  Регистрация
+                </Typography>
               </Box>
-              <Typography
-                component="h1"
-                variant="h5"
-                marginTop="21px"
-                sx={{
-                  textAlign: 'center',
-                  fontSize: '24px',
-                  lineHeight: '32px',
-                  fontWeight: '600',
-                }}
-              >
-                Регистрация
-              </Typography>
+              <SignupForm />
             </Box>
-            <SignupForm />
           </Box>
         </Box>
-      </Box>
-    </>
-  )),
+      </>
+    );
+  }),
 );
 
 export default Signup;
