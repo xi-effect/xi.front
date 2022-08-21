02073f2507d0b0755273577ed391fa5604d6fa92
unknownproperty <igor.buksheff@yandex.ru>
2022-08-22 00:57:10 +0300

    fix: build and remove support page

--- kit/Navigation/Sidebar/CommunityItem.tsx
+++ kit/Navigation/Sidebar/CommunityItem.tsx
@@ -15,7 +15,12 @@ const CommunityItem: React.FC<CommunityItemType> = (props) => {
     <Draggable draggableId={`sidebar-communities-list-id-${item.id}`} index={index}>
       {(provided) => (
         <Stack ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
-          <IButton tooltip={item.name} typography={item.name[0].toUpperCase()} href={`/community/${item.id}`} isBefore />
+          <IButton
+            tooltip={item.name}
+            typography={item.name[0].toUpperCase()}
+            href={`/community/${item.id}`}
+            isBefore
+          />
         </Stack>
       )}
     </Draggable>
