7e21ee8b5459b37e9598f24a4be70a8c26eeac2f
unknownproperty <igor.buksheff@yandex.ru>
2022-08-22 00:48:00 +0300

    feat: Sidebar new design

--- kit/Navigation/Sidebar/CommunityItem.tsx
+++ kit/Navigation/Sidebar/CommunityItem.tsx
@@ -1,45 +1,21 @@
 import React from 'react';
 import { Draggable } from 'react-beautiful-dnd';
-import { Stack, IconButton, Tooltip } from '@mui/material';
-import { motion } from 'framer-motion';
-import { useRouter } from 'next/router';
-import { blue, green, orange, red, indigo } from '@mui/material/colors';
-import Avatar from 'boring-avatars';
+import { Stack } from '@mui/material';
+import IButton from './IButton';
 
 type CommunityItemType = {
   item: any;
   index: any;
 };
 
-const colors = [blue[100], green[600], orange[400], indigo[300], red[700]];
-
 const CommunityItem: React.FC<CommunityItemType> = (props) => {
   const { item, index } = props;
 
-  const router = useRouter();
   return (
     <Draggable draggableId={`sidebar-communities-list-id-${item.id}`} index={index}>
       {(provided) => (
         <Stack ref={provided.innerRef} {...provided.draggableProps} {...provided.dragHandleProps}>
-          <Tooltip enterDelay={500} leaveDelay={0} placement="right" title={item.name}>
-            <IconButton
-              component={motion.li}
-              onClick={() => {
-                router.push(`/community/${item.id}`);
-              }}
-              sx={{
-                borderRadius: '21px',
-                height: '50px',
-                width: '50px',
-                p: 0,
-                '&:hover': {
-                  bgcolor: 'primary.dark',
-                },
-              }}
-            >
-              <Avatar size={64} name={`${item.label}${item.id}`} variant="beam" colors={colors} />
-            </IconButton>
-          </Tooltip>
+          <IButton tooltip={item.name} typography={item.name[0].toUpperCase()} href={`/community/${item.id}`} isBefore />
         </Stack>
       )}
     </Draggable>
