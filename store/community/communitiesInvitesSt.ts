ed5da499b4f7d9ed46143acdd425027e9d76501c
Pavel <93088269+KhaninPasha0488@users.noreply.github.com>
2022-08-26 16:02:37 +0300

    fix : communiti change to TS

--- store/community/communitiesInvitesSt.ts
+++ store/community/communitiesInvitesSt.ts
@@ -1,10 +1,13 @@
 import { action, observable, makeObservable } from 'mobx';
+import RootStore from "../rootStore";
 
 class CommunitiesInvitesSt {
   // `this` from rootstore passed to the constructor and we can
   // assign it to a variable accessible in this class called
   // `rootStore`. Therefore, we can access other store like
   // useStore for e.g (this.rootStore.userStore)
+  rootStore: RootStore;
+
   constructor(rootStore) {
     this.rootStore = rootStore;
     makeObservable(this);
--- store/home/homeSt.ts
+++ store/home/homeSt.ts
@@ -1,10 +1,13 @@
 import { observable, makeObservable } from 'mobx';
+import RootStore from "../rootStore";
 
 class HomeSt {
   // `this` from rootstore passed to the constructor and we can
   // assign it to a variable accessible in this class called
   // `rootStore`. Therefore, we can access other store like
   // useStore for e.g (this.rootStore.userStore)
+  rootStore: RootStore;
+
   constructor(rootStore) {
     this.rootStore = rootStore;
     makeObservable(this);
