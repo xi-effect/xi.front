da2cf5e237b8c2930531406ba92522da9febac89
qzart45hi <qwert45hi@yandex.ru>
2022-08-30 00:50:13 +0300

    feat: pixel-perfect-ish error page

--- components/Error/ErrorPage.tsx
+++ components/Error/ErrorPage.tsx
@@ -1,8 +1,9 @@
-import { Button, Stack, Typography } from '@mui/material'; // useMediaQuery
+import { Box, Button, Stack, Typography } from '@mui/material'; // useMediaQuery
 import { useRouter } from 'next/router';
 import { motion } from 'framer-motion';
 import React from 'react';
-import { errorCode } from 'texts/errorMessages/errorMessages';
+import { errorCode, errorMessages } from 'texts/errorMessages/errorMessages';
+import XiLogo from 'kit/XiLogo';
 
 type ErrorPageProps = {
   code: errorCode;
@@ -17,78 +18,93 @@ export default function ErrorPage({ code }: ErrorPageProps) {
   return (
     <Stack
       component={motion.div}
-      initial={{ opacity: 0 }}
+      initial={{ opacity: 1 }}
       animate={{ opacity: 1 }}
       transition={{ delay: 0.4, duration: 2 }}
       direction="column"
-      justifyContent="flex-start"
-      alignItems="center"
       sx={{
         zIndex: 1,
         margin: 0,
         overflow: 'auto',
         height: '100vh',
+        width: '100%',
+        paddingX: '100px',
         bgcolor: 'background.main',
       }}
     >
-      <Stack direction="row" justifyContent="center" alignItems="center">
-        Header
-      </Stack>
       <Stack
         direction="row"
         justifyContent="center"
         alignItems="center"
         sx={{
-          height: '100%',
+          width: '100%',
+          height: '32px',
+          marginTop: '111px',
         }}
       >
-        <Stack
-          direction="column"
-          justifyContent="center"
-          alignItems="center"
-          spacing={4}
+        <Box style={{ flexGrow: 1 }}>
+          <XiLogo />
+        </Box>
+        <Typography variant='body1' color='gray.80' sx={{ lineHeight: '32px' }}>
+          ссылки тут
+        </Typography>
+      </Stack>
+      <Stack
+        direction="row"
+        justifyContent="left"
+        sx={{
+          marginTop: '153px',
+          height: '220px'
+        }}
+      >
+        <Typography
           sx={{
-            mr: 32,
+            fontSize: '220px',
+            lineHeight: '220px',
+            fontWeight: 600,
           }}
         >
-          <Typography variant="h1">{code}</Typography>
-          <Button
-            onClick={() => {
-              router.push({
-                pathname: '/',
-              });
-            }}
+          {code}
+        </Typography>
+        <Typography
+          sx={{
+            fontSize: '32px',
+            lineHeight: '42px',
+            fontWeight: 400,
+            marginTop: '148px',
+            marginLeft: '56px',
+          }}
+        >
+          {errorMessages[code]}
+        </Typography>
+      </Stack>
+      <Stack
+        direction="row"
+        justifyContent="left"
+        alignItems="center"
+        sx={{
+          marginTop: '72px',
+        }}
+      >
+        <Button
+          variant="contained"
+          onClick={() => router.push('/')}
+          sx={{
+            height: '64px',
+            width: '201px',
+            textTransform: 'none',
+          }}
+        >
+          <Typography
             sx={{
-              '&.MuiButton-root': {
-                width: '289px',
-                height: '60px',
-                color: 'text.main',
-                bgcolor: 'primary.main',
-                borderRadius: '88px',
-                boxShadow: 24,
-                '&:hover': {
-                  bgcolor: 'primary.main',
-                },
-              },
-              mt: '20px',
-              mb: '20px',
+              fontSize: '24px',
+              lineHeight: '32px',
+              fontWeight: 500,
             }}
           >
-            <Typography
-              sx={{
-                '&.MuiTypography-root': {
-                  fontWeight: 500,
-                  fontSize: '18px',
-                  lineHeight: '25px',
-                },
-                ml: '24px',
-                color: 'text.primary',
-              }}
-            >
-              Вернуться на главную
-            </Typography>
-          </Button>
-        </Stack>
+            На главную
+          </Typography>
+        </Button>
       </Stack>
     </Stack>
   );
