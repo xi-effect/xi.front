6a4d7268f7f4ed9e8229b5f82b727c1290c73f80
qzart45hi <qwert45hi@yandex.ru>
2022-09-04 22:08:52 +0300

    feat: separate breakpoints

--- components/Error/ErrorPage.tsx
+++ components/Error/ErrorPage.tsx
@@ -3,87 +3,24 @@ import { NextRouter, useRouter } from 'next/router';
 import { motion } from 'framer-motion';
 import { errorCode, errorMessages } from 'texts/errorMessages/errorMessages';
 import Header from 'components/Landing/Header';
-
-type ScreenSize = 'min480' | 'min1000' | 'min1336' | 'min1920' | 'max1920';
-type ScreenToString = { [key in ScreenSize]: string }; // eslint-disable-line no-unused-vars
+import {
+  buttonFontSize,
+  buttonHeight,
+  buttonMarginTop,
+  buttonWidth,
+  mainFontSize,
+  mainMarginTop,
+  ScreenSize,
+  secondaryFontSize,
+  secondaryLineHeight,
+  secondaryMarginTop,
+} from './breakpoints';
 
 type ErrorPageProps = {
   code: errorCode;
 };
 
 export default function ErrorPage({ code }: ErrorPageProps) {
-  const mainFontSize: ScreenToString = {
-    min480: '100px',
-    min1000: '160px',
-    min1336: '160px',
-    min1920: '220px',
-    max1920: '220px',
-  };
-
-  const mainMarginTop: ScreenToString = {
-    min480: '65px', // + 5 instead of line height of main
-    min1000: '78px',
-    min1336: '78px',
-    min1920: '94px',
-    max1920: '94px',
-  };
-
-  const secondaryFontSize: ScreenToString = {
-    min480: '16px',
-    min1000: '24px',
-    min1336: '24px',
-    min1920: '32px',
-    max1920: '32px',
-  };
-
-  const secondaryLineHeight: ScreenToString = {
-    min480: '32px',
-    min1000: '32px',
-    min1336: '32px',
-    min1920: '42px',
-    max1920: '42px',
-  };
-
-  const secondaryMarginTop: ScreenToString = {
-    min480: '5px', // + 5 instead of line height of main
-    min1000: '24px',
-    min1336: '24px',
-    min1920: '32px',
-    max1920: '32px',
-  };
-
-  const buttonFontSize: ScreenToString = {
-    min480: '18px',
-    min1000: '24px',
-    min1336: '24px',
-    min1920: '24px',
-    max1920: '24px',
-  };
-
-  const buttonHeight: ScreenToString = {
-    min480: '48px',
-    min1000: '64px',
-    min1336: '64px',
-    min1920: '64px',
-    max1920: '64px',
-  };
-
-  const buttonWidth: ScreenToString = {
-    min480: '167px',
-    min1000: '201px',
-    min1336: '201px',
-    min1920: '201px',
-    max1920: '201px',
-  };
-
-  const buttonMarginTop: ScreenToString = {
-    min480: '48px',
-    min1000: '64px',
-    min1336: '64px',
-    min1920: '100px',
-    max1920: '100px',
-  };
-
   // скопировано из Main в pages/index.tsx
   const mobile1920: boolean = useMediaQuery((theme: Theme) => theme.breakpoints.down(1920));
   const mobile1336: boolean = useMediaQuery((theme: Theme) => theme.breakpoints.down(1336));
