8832bb47543aabcdf4c77c9a69d2b2c89e808eed
s2000promax <s2000promax@gmail.com>
2022-09-08 17:18:31 +0400

    feat: refactor hook useFiles

--- pages/research/index.jsx
+++ pages/research/index.jsx
@@ -1,14 +1,50 @@
 import React, { useRef, useState } from 'react';
-import { Button, Stack, Typography, LinearProgress } from '@mui/material';
+import {
+  Button,
+  Stack,
+  Typography,
+  LinearProgress,
+  FormControl,
+  FormLabel,
+  RadioGroup,
+  FormControlLabel, Radio, Box
+} from '@mui/material';
 import { AttachFile, TextSnippetOutlined, Clear } from '@mui/icons-material';
-import { usePostFetcher } from 'kit/useFetcher';
+import { useGetFile, usePostFiles, useDeleteFiles } from 'utils/useFiles';
+import TextField from '@mui/material/TextField';
 
 const Research = () => {
   const filePicker = useRef(null);
   const [filesArray, setFilesArray] = useState([]);
 
-  const { handlePost, fileSizeFormat, isLoading, errors, notUploaded} = usePostFetcher();
-  console.log(errors, notUploaded);
+  const [formType, setFormType] = useState('post');
+  const [getFileName, setGetFileName] = useState('');
+  const [deleteFileName, setDeleteGetFileName] = useState('');
+
+
+  const { handleGetFile, downloadURL, error: getError, isLoading } = useGetFile();
+  const { handlePostFile, fileSizeFormat, isUploading, error: postError, notUploaded } = usePostFiles();
+  const { handleDeleteFile, error: deleteError, isDeleting } = useDeleteFiles();
+
+  if (notUploaded) {
+    console.log('Файлы не были загружена на сервер:', notUploaded);
+  }
+
+  if (formType === 'post') {
+    console.log('Error post:', postError);
+  }
+
+  const handleFormTypeChange = (event) => {
+    setFormType(event.target.value);
+  };
+
+  const handleFormGetFileNameChange = (event) => {
+    setGetFileName(event.target.value);
+  };
+
+  const handleFormDeleteFileNameChange = (event) => {
+    setDeleteGetFileName(event.target.value);
+  };
 
   const handleInputFile = (event) => {
     setFilesArray(prevState => [...prevState, ...event.target.files]);
@@ -24,94 +60,293 @@ const Research = () => {
 
   return (
     <>
-      <input
-        onChange={handleInputFile}
-        type="file"
-        ref={filePicker}
-        multiple
-        style={{ position: 'absolute', visibility: 'hidden' }}
-      />
-      <Stack
-        alignItems="center"
-        justifyContent="center"
-        sx={{
-          width: '100%',
-          height: '100vh',
-          backgroundColor: '#ECEFFF',
-        }}
-      >
+      <FormControl sx={{ position: 'absolute' }} >
+        <FormLabel id="demo-controlled-radio-buttons-group">API files</FormLabel>
+        <RadioGroup
+          aria-labelledby="demo-controlled-radio-buttons-group"
+          name="controlled-radio-buttons-group"
+          value={formType}
+          onChange={handleFormTypeChange}
+        >
+          <FormControlLabel value="get" control={<Radio />} label="get" />
+          <FormControlLabel value="post" control={<Radio />} label="post" />
+          <FormControlLabel value="delete" control={<Radio />} label="delete" />
+        </RadioGroup>
+      </FormControl>
+       {
+          formType === 'get'
+        &&
         <Stack
+          alignItems="center"
+          justifyContent="center"
           sx={{
-            width: '508px',
-            borderRadius: '8px',
-            backgroundColor: '#FFFFFF',
+            width: '100%',
+            height: '100vh',
+            backgroundColor: '#ECEFFF',
           }}
         >
-          <Stack direction="row" justifyContent="space-between" sx={{ m: '24px 24px 12px 24px' }}>
-            <Stack direction="row" alignItems="flex-end">
-              <Typography variant="h5"><strong>Решение</strong></Typography>
-              <Typography variant="subtitle2" color="#999999" sx={{ ml: '5px', mb: '4px' }}>до 14 сен 22</Typography>
-            </Stack>
-            <Typography variant="subtitle1"
-                        sx={{
-                          borderRadius: '8px',
-                          backgroundColor: '#B0F9CE',
-                          color: '#11743A',
-                          p: '4px 14px 4px 14px'
-                        }}
-            >
-              Назначено
-            </Typography>
-          </Stack>
-
-          {filesArray
-            .map((file, index) =>
-              (
-                <FileItemComponent
-                  key={index}
-                  id={index}
-                  handleFileDelete={handleFileDelete}
-                  fileName={file.name}
-                  fileSize={fileSizeFormat(file.size)}
-                  isLoading={isLoading && index === 0}
-                />
-              ))}
-          <Stack
-            direction="row"
-            justifyContent="space-between"
-            spacing="12px"
-            sx={{
-              m: '24px 24px 0 24px'
-            }}
-          >
-            <Button onClick={handlePick} variant="outlined" startIcon={<AttachFile/>}
-                    sx={{ width: '50%', height: '48px' }}>
-              Прикрепить
-            </Button>
-            <Button variant="outlined" startIcon={<TextSnippetOutlined/>} sx={{ width: '50%', height: '48px' }}>
-              Редактор
-            </Button>
-          </Stack>
           <Stack
             sx={{
-              height: '48px',
-              m: '12px 24px 24px 24px'
+              width: '508px',
+              borderRadius: '8px',
+              backgroundColor: '#FFFFFF',
             }}
           >
-            <Button
-              onClick={() => handlePost(filesArray)}
-              variant="contained"
+            <Stack
+              direction="row"
+              alignItems="center"
+              justifyContent="space-between"
               sx={{
-                width: '100%',
+                height: '372px', m: '24px 24px 0 24px',
+                border: '1px solid #E6E6E6',
                 borderRadius: '8px',
-                backgroundColor: '#445AFF',
-                color: '#FFFFFF',
-                fontSize: '18px',
-                textTransform: 'none',
-              }}>Сдать</Button>
+              }}
+            >
+              <Box
+                textAlign="center"
+                sx={{
+                  width: '300px',
+                  minHeight: '300px'
+                }}
+              >
+                {downloadURL && !getError ?
+                  <img
+                    alt=""
+                    src={downloadURL}
+                    style={{
+                      maxWidth: '350px',
+                      height: 'auto'
+                    }}
+                  />
+                 : <Typography variant="h6" textAlign="center">{getError}</Typography>
+                }
+              </Box>
+            </Stack>
+            {
+              isLoading &&
+              <Stack
+                sx={{
+                  m: '-8px 32px 0 32px',
+                  border: '1px solid #E6E6E6',
+                  borderRadius: '8px',
+                }}
+              >
+                <LinearProgress/>
+              </Stack>
+            }
+
+            <Stack
+              direction="row"
+              justifyContent="space-between"
+              sx={{
+                m: '24px 24px 10px 24px'
+              }}
+            >
+              <TextField
+                variant="outlined"
+                sx={{ width: '100%', height: '48px' }}
+                onChange={handleFormGetFileNameChange}
+                value={getFileName}
+                placeholder="Введите имя файла"
+              />
+            </Stack>
+            <Stack
+              sx={{
+                height: '48px',
+                m: '12px 24px 24px 24px'
+              }}
+            >
+              <Button
+                onClick={() => handleGetFile(getFileName)}
+                variant="contained"
+                sx={{
+                  width: '100%',
+                  borderRadius: '8px',
+                  backgroundColor: '#445AFF',
+                  color: '#FFFFFF',
+                  fontSize: '18px',
+                  textTransform: 'none',
+                }}>Загрузить</Button>
+            </Stack>
           </Stack>
         </Stack>
-      </Stack>
+      }
+
+      {
+        formType === 'post'
+        && <>
+          <input
+            onChange={handleInputFile}
+            type="file"
+            ref={filePicker}
+            multiple
+            style={{ position: 'absolute', visibility: 'hidden' }}
+          />
+          <Stack
+            alignItems="center"
+            justifyContent="center"
+            sx={{
+              width: '100%',
+              height: '100vh',
+              backgroundColor: '#ECEFFF',
+            }}
+          >
+            <Stack
+              sx={{
+                width: '508px',
+                borderRadius: '8px',
+                backgroundColor: '#FFFFFF',
+              }}
+            >
+              <Stack direction="row" justifyContent="space-between" sx={{ m: '24px 24px 12px 24px' }}>
+                <Stack direction="row" alignItems="flex-end">
+                  <Typography variant="h5"><strong>Решение</strong></Typography>
+                  <Typography variant="subtitle2" color="#999999" sx={{ ml: '5px', mb: '4px' }}>до 14 сен 22</Typography>
+                </Stack>
+                <Typography variant="subtitle1"
+                            sx={{
+                              borderRadius: '8px',
+                              backgroundColor: '#B0F9CE',
+                              color: '#11743A',
+                              p: '4px 14px 4px 14px'
+                            }}
+                >
+                  Назначено
+                </Typography>
+              </Stack>
+
+              {filesArray
+                .map((file, index) =>
+                  (
+                    <FileItemComponent
+                      key={index}
+                      id={index}
+                      handleFileDelete={handleFileDelete}
+                      fileName={file.name}
+                      fileSize={fileSizeFormat(file.size)}
+                      isUploading={isUploading && index === 0}
+                    />
+                  ))}
+              <Stack
+                direction="row"
+                justifyContent="space-between"
+                spacing="12px"
+                sx={{
+                  m: '24px 24px 0 24px'
+                }}
+              >
+                <Button onClick={handlePick} variant="outlined" startIcon={<AttachFile/>}
+                        sx={{ width: '50%', height: '48px' }}>
+                  Прикрепить
+                </Button>
+                <Button variant="outlined" startIcon={<TextSnippetOutlined/>} sx={{ width: '50%', height: '48px' }}>
+                  Редактор
+                </Button>
+              </Stack>
+              <Stack
+                sx={{
+                  height: '48px',
+                  m: '12px 24px 24px 24px'
+                }}
+              >
+                <Button
+                  onClick={() => handlePostFile(filesArray)}
+                  variant="contained"
+                  sx={{
+                    width: '100%',
+                    borderRadius: '8px',
+                    backgroundColor: '#445AFF',
+                    color: '#FFFFFF',
+                    fontSize: '18px',
+                    textTransform: 'none',
+                  }}>Сдать</Button>
+              </Stack>
+            </Stack>
+          </Stack>
+        </>
+      }
+      {
+        formType === 'delete'
+        &&
+        <Stack
+          alignItems="center"
+          justifyContent="center"
+          sx={{
+            width: '100%',
+            height: '100vh',
+            backgroundColor: '#ECEFFF',
+          }}
+        >
+          <Stack
+            sx={{
+              width: '508px',
+              borderRadius: '8px',
+              backgroundColor: '#FFFFFF',
+            }}
+          >
+            <Stack
+              direction="row"
+              alignItems="center"
+              justifyContent="space-between"
+              sx={{
+                height: '72px', m: '24px 24px 0 24px',
+                border: '1px solid #E6E6E6',
+                borderRadius: '8px',
+              }}
+            >
+              {
+                deleteError && <Typography variant="h6" textAlign="center">{deleteError}</Typography>
+              }
+            </Stack>
+            {
+              isDeleting &&
+              <Stack
+                sx={{
+                  m: '-8px 32px 0 32px',
+                  border: '1px solid #E6E6E6',
+                  borderRadius: '8px',
+                }}
+              >
+                <LinearProgress/>
+              </Stack>
+            }
+            <Stack
+              direction="row"
+              justifyContent="space-between"
+              spacing="12px"
+              sx={{
+                m: '24px 24px 10px 24px'
+              }}
+            >
+              <TextField
+                variant="outlined"
+                sx={{ width: '100%', height: '48px' }}
+                onChange={handleFormDeleteFileNameChange}
+                value={deleteFileName}
+                placeholder="Введите идентификатор файла"
+              />
+            </Stack>
+            <Stack
+              sx={{
+                height: '48px',
+                m: '12px 24px 24px 24px'
+              }}
+            >
+              <Button
+                onClick={() => handleDeleteFile(deleteFileName)}
+                variant="contained"
+                sx={{
+                  width: '100%',
+                  borderRadius: '8px',
+                  backgroundColor: '#445AFF',
+                  color: '#FFFFFF',
+                  fontSize: '18px',
+                  textTransform: 'none',
+                }}>Удалить</Button>
+            </Stack>
+          </Stack>
+        </Stack>
+      }
     </>
   );
 };
@@ -120,7 +355,7 @@ export default Research;
 
 const FileItemComponent = (props) => {
   // eslint-disable-next-line react/prop-types
-  const { id, fileName, fileSize, handleFileDelete, isLoading } = props;
+  const { id, fileName, fileSize, handleFileDelete, isUploading } = props;
 
   return (
     <>
@@ -149,7 +384,7 @@ const FileItemComponent = (props) => {
         </Stack>
       </Stack>
       {
-        isLoading &&
+        isUploading &&
         <Stack
           sx={{
             m: '-8px 32px 0 32px',
@@ -157,10 +392,9 @@ const FileItemComponent = (props) => {
             borderRadius: '8px',
           }}
         >
-          <LinearProgress />
+          <LinearProgress/>
         </Stack>
       }
-
     </>
   );
 };
---
+++ utils/useFiles.ts
@@ -0,0 +1,184 @@
+import { useState } from 'react';
+
+const baseUrl: string = `${process.env.NEXT_PUBLIC_SERVER_URL}/files/`;
+
+interface IFile {
+  name: string,
+  size: number,
+  type: string
+}
+
+interface IResult {
+  url?: string,
+  error?: string,
+}
+
+const fileSizeFormat = (fileSize: number): string => {
+  const stringLength: number = fileSize.toString().length;
+  if (stringLength > 9) {
+    return `${(fileSize / 1000 ** 3).toFixed(1)} ГБ`;
+  }
+  if (stringLength > 6) {
+    return `${(fileSize / 1000 ** 2).toFixed(1)} МБ`;
+  }
+  if (stringLength > 3) {
+    return `${(fileSize / 1000).toFixed(1)} КБ`;
+  }
+  return `${fileSize} Б`;
+};
+
+const getFile = async (fileName): Promise<IResult> => {
+  try {
+    const response = await fetch(`${baseUrl + fileName}/`);
+
+    if (response.status === 200) {
+      const dataBlob = await response.blob();
+      return { url: URL.createObjectURL(dataBlob) };
+    }
+    return { error: 'Ошибка получения файла' };
+  } catch (e) {
+    return { error: 'Ошибка получения файла' };
+  }
+};
+
+export const useGetFile: any = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [downloadURL, setDownloadURL] = useState<string>();
+  const [error, setError] = useState<string>();
+
+  const handleGetFile = async (fileName: string) => {
+    if (!fileName) {
+      setError('Имя файла не может быть пустым');
+      return;
+    }
+    setIsLoading(true);
+
+    const data = await getFile(fileName);
+    if (data.url) {
+      setDownloadURL(data.url);
+      setError(undefined);
+    } else if (data.error) {
+      setError(data.error);
+    }
+    setIsLoading(false);
+  };
+
+  return { handleGetFile, downloadURL, error, isLoading };
+};
+
+const postFile = async (file) => {
+
+  try {
+    const formData = new FormData();
+    formData.append('file', file);
+
+    const response = await fetch(baseUrl,
+      {
+        method: 'POST',
+        body: formData,
+        credentials: "include",
+        mode: "cors",
+      }
+    );
+
+    if (response.status === 200) {
+      const data = await response.json();
+      // console.log(data);
+      return { ...data };
+    }
+  } catch (e) {
+    return { error: 'Файл не был загружен на сервер' };
+  }
+  return { error: 'Файл не был загружен на сервер' };
+};
+
+export const usePostFiles: any = () => {
+  const [isUploading, setIsUploading] = useState<boolean>(false);
+  const [error, setError] = useState<string>();
+  const [notUploaded, setNotUploaded] = useState<any[]>();
+
+  const validate = (files: IFile[]): boolean => {
+    if (!files.length) {
+      setError('Отсутсвуют файлы к отправке');
+      return false;
+    }
+
+    for (const file of files) {
+      if (file.size > 4 * 1000 ** 2) {
+        setError('Размер файла превышает 4 МБ');
+        return false;
+      }
+    }
+    setError(undefined);
+    return true;
+  };
+
+  const postFiles = async (files) => {
+    const currentFile = files.shift();
+
+    const data = await postFile(currentFile);
+    // console.log(data);
+    if (data.error) {
+      setNotUploaded(prevState => prevState ? [...prevState, currentFile] : [currentFile]);
+    }
+    if (!files.length) return files;
+
+    return postFiles(files);
+  };
+
+  const handlePostFile = async (filesToPost: IFile[]) => {
+    if (!validate(filesToPost)) {
+      return;
+    }
+
+    setIsUploading(true);
+    const data = await postFiles(filesToPost);
+    if (data.error) {
+      setError(data.error);
+    } else setError(undefined);
+    setIsUploading(false);
+  };
+
+  return { handlePostFile, fileSizeFormat, isUploading, error, notUploaded };
+};
+
+const deleteFile = async (fileId): Promise<IResult> => {
+  try {
+    const response = await fetch(`${baseUrl}manager/${fileId}/`,
+      {
+        method: 'DELETE',
+        credentials: "include",
+        mode: "cors",
+      }
+    );
+
+    if (response.status === 200) {
+      const data = await response.json();
+      return { ...data };
+    }
+    return { error: 'Ошибка удаления файла' };
+  } catch (e) {
+    return { error: 'Ошибка удаления файла' };
+  }
+};
+
+export const useDeleteFiles: any = () => {
+  const [isDeleting, setIsDeleting] = useState<boolean>(false);
+  const [error, setError] = useState<string>();
+
+  const handleDeleteFile = async (fileId: string) => {
+    if (!fileId) {
+      setError('Идентификатор файла не может быть пустым');
+      return;
+    }
+
+    setIsDeleting(true);
+    const data = await deleteFile(fileId);
+    if (data.error) {
+      setError(data.error);
+    } else setError(undefined);
+    setIsDeleting(false);
+  };
+
+  return { handleDeleteFile, error, isDeleting };
+};
