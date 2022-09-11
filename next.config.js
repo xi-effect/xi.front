8d9477e4c86968e71f3b92caf56a76bca200d42d
unknownproperty <igor.buksheff@yandex.ru>
2022-09-11 14:13:38 +0300

    feat: update next config

--- next.config.js
+++ next.config.js
@@ -1,27 +1,42 @@
 // Не поддавайтесь соблазну использовать здесь import
-const withPWA = require("next-pwa");
-const runtimeCaching = require("next-pwa/cache");
+const withPWA = require('next-pwa');
+const runtimeCaching = require('next-pwa/cache');
 
 module.exports = withPWA({
-  experimental: { esmExternals: false },
+  experimental: { esmExternals: true },
+  compiler: {
+    removeConsole: process.env.NODE_ENV !== 'development',
+  },
   reactStrictMode: true,
+  swcMinify: true,
   images: {
-    domains: ["xieffect.pythonanywhere.com", "cdn.discordapp.com", "localhost:3000", "localhost:5000", "xieffect.ru:5000", "xieffect.ru", "www.youtube.com"],
+    domains: [
+      'xieffect.pythonanywhere.com',
+      'cdn.discordapp.com',
+      'localhost:3000',
+      'localhost:5000',
+      'xieffect.ru:5000',
+      'xieffect.ru',
+      'www.youtube.com',
+    ],
   },
   pwa: {
-    disable: process.env.NODE_ENV === 'development',
     dest: 'public',
     register: true,
-    skipWaiting: true,
     runtimeCaching,
-    sw: "/sw.js",
+    mode: 'production',
+    reloadOnOnline: true,
+    cacheOnFrontEndNav: true,
+    disable: process.env.NODE_ENV === 'development',
+    skipWaiting: true,
+    sw: '/sw.js',
     buildExcludes: [
       /middleware-manifest\.json$/,
       /_middleware\.js$/,
       /_middleware\.js\.map$/,
       /middleware-runtime\.js$/,
       /server\/pages\/_middleware\.js$/,
-    ]
+    ],
   },
   webpack(config) {
     config.module.rules.push({
