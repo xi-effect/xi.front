ae2cffa918095ea44877e6f99ead260e54a919ff
Mr.G3ntl3man <archi3run@gmail.com>
2022-10-02 22:13:43 +0300

    feat: styles for layout

---
+++ kit/Editor/common/styles.ts
@@ -0,0 +1,36 @@
+export const menuStyles = {
+  ml: '8px',
+  '& .MuiBackdrop-invisible': {
+    backgroundColor: 'rgba(0,0,0,0)',
+  },
+  '& .MuiMenuItem-root:hover': {
+    backgroundColor: '#ECEFFF',
+    borderRadius: '4px',
+
+    '& .MuiTypography-body1': {
+      color: '#445AFF',
+    },
+  },
+  '& .MuiMenu-list': {
+    p: '8px',
+  },
+  '& .MuiPaper-elevation': {
+    boxShadow: '0',
+    borderRadius: '8px',
+    border: '1px solid #E6E6E6',
+  },
+  '& .MuiMenuItem-root': {
+    transition: 'background-color 0.2s ease-in, color 0.2s ease-in ',
+  },
+};
+
+export const menuDelete = {
+  '& .MuiMenuItem-root:hover': {
+    backgroundColor: '#FEEAEA',
+    borderRadius: '4px',
+
+    '& .MuiTypography-body1': {
+      color: '#F42D2D',
+    },
+  },
+};
