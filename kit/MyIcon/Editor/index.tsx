84a29b6e2d1c707f8282b6796fa5373efe9a4aed
Mr.G3ntl3man <archi3run@gmail.com>
2022-10-17 22:55:28 +0300

    fix: add new icons

--- kit/MyIcon/Editor/index.tsx
+++ kit/MyIcon/Editor/index.tsx
@@ -1,22 +1,22 @@
-import * as React from 'react';
+import React from 'react';
 import { Stack } from '@mui/material';
-import Text from './Text';
 import H1 from './H1';
 import H2 from './H2';
 import H3 from './H3';
-import NumList from './NumList';
-import MarkList from './MarkList';
+import Text from './Text';
 import Quote from './Quote';
-import Divider from './Divider';
 import Image from './Image';
 import Video from './Video';
-import FileEditor from './File';
-import Delete from './Delete';
 import Arrow from './Arrow';
+import Delete from './Delete';
+import Divider from './Divider';
+import FileEditor from './File';
+import NumList from './NumList';
+import MarkList from './MarkList';
 
 type EditorIconT = {
   name: string;
-  color: string;
+  color?: string;
 };
 
 export const IconWrap: React.FC = ({ children, ...props }) => (
@@ -39,20 +39,23 @@ export const IconWrap: React.FC = ({ children, ...props }) => (
 
 const EditorIcon: React.FC<EditorIconT> = ({ name, color }) => {
   const icon = {
-    text: <Text color={color} />,
     h1: <H1 color={color} />,
     h2: <H2 color={color} />,
     h3: <H3 color={color} />,
+    text: <Text color={color} />,
     ol: <NumList color={color} />,
     ul: <MarkList color={color} />,
     quote: <Quote color={color} />,
-    divider: <Divider color={color} />,
     image: <Image color={color} />,
     video: <Video color={color} />,
-    fileEditor: <FileEditor color={color} />,
     delete: <Delete color={color} />,
-    arrowUp: <Arrow up color={color} />,
     arrowDown: <Arrow color={color} />,
+    divider: <Divider color={color} />,
+    arrowUp: <Arrow up color={color} />,
+    fileEditor: <FileEditor color={color} />,
+    imageMedium: <Image medium color={color} />,
+    videoMedium: <Video medium color={color} />,
+    fileEditorMedium: <FileEditor medium color={color} />,
   };
 
   return icon[name];
