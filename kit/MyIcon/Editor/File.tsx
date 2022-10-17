5c2f7344255250a621f53093673fd38168c35132
Mr.G3ntl3man <archi3run@gmail.com>
2022-10-17 22:51:13 +0300

    fix: add medium size icon

--- kit/MyIcon/Editor/File.tsx
+++ kit/MyIcon/Editor/File.tsx
@@ -1,26 +1,58 @@
+import React from 'react';
 import { IconWrap } from './index';
 
 type Props = {
   color?: string;
+  medium?: boolean;
 };
 
-const File = ({ color = '#333' }: Props) => (
+const File: React.FC<Props> = ({ color = '#333', medium }) => (
   <IconWrap>
-    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path
-        d="M8 20H16C17.6569 20 19 18.6569 19 17V9.24264C19 8.44699 18.6839 7.68393 18.1213 7.12132L15.8787 4.87868C15.3161 4.31607 14.553 4 13.7574 4H8C6.34315 4 5 5.34315 5 7V17C5 18.6569 6.34315 20 8 20Z"
-        stroke={color}
-        strokeWidth="2"
-        strokeLinecap="round"
-      />
-      <path
-        d="M14 4V7C14 8.10457 14.8954 9 16 9H19"
-        stroke={color}
-        strokeWidth="2"
-        strokeLinecap="round"
-      />
-      <path d="M9 12H11.75M9 16H13.75" stroke={color} strokeWidth="2" strokeLinecap="round" />
-    </svg>
+    {medium ? (
+      <svg
+        width="48"
+        height="48"
+        viewBox="0 0 48 48"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M16.5 40.5H31.5C34.8137 40.5 37.5 37.8137 37.5 34.5V18.364C37.5 17.1705 37.0259 16.0259 36.182 15.182L29.818 8.81802C28.9741 7.97411 27.8295 7.5 26.636 7.5H16.5C13.1863 7.5 10.5 10.1863 10.5 13.5V34.5C10.5 37.8137 13.1863 40.5 16.5 40.5Z"
+          stroke="#445AFF"
+          strokeWidth="3"
+          strokeLinecap="round"
+        />
+        <path
+          d="M26.25 7.5V14.25C26.25 16.7353 28.2647 18.75 30.75 18.75H37.5"
+          stroke="#445AFF"
+          strokeWidth="3"
+          strokeLinecap="round"
+        />
+        <path d="M16.5 27H24M16.5 33H30" stroke="#445AFF" strokeWidth="3" strokeLinecap="round" />
+      </svg>
+    ) : (
+      <svg
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M8 20H16C17.6569 20 19 18.6569 19 17V9.24264C19 8.44699 18.6839 7.68393 18.1213 7.12132L15.8787 4.87868C15.3161 4.31607 14.553 4 13.7574 4H8C6.34315 4 5 5.34315 5 7V17C5 18.6569 6.34315 20 8 20Z"
+          stroke={color}
+          strokeWidth="2"
+          strokeLinecap="round"
+        />
+        <path
+          d="M14 4V7C14 8.10457 14.8954 9 16 9H19"
+          stroke={color}
+          strokeWidth="2"
+          strokeLinecap="round"
+        />
+        <path d="M9 12H11.75M9 16H13.75" stroke={color} strokeWidth="2" strokeLinecap="round" />
+      </svg>
+    )}
   </IconWrap>
 );
 
--- kit/MyIcon/Editor/Video.tsx
+++ kit/MyIcon/Editor/Video.tsx
@@ -1,20 +1,54 @@
+import React from 'react';
 import { IconWrap } from './index';
 
 type Props = {
   color?: string;
+  medium?: boolean;
 };
 
-const Video = ({ color = '#333' }: Props) => (
+const Video: React.FC<Props> = ({ color = '#333', medium }) => (
   <IconWrap>
-    <svg width="12" height="14" viewBox="0 0 12 14" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path
-        d="M10.3905 7.8779C11.0854 7.49888 11.0854 6.50112 10.3905 6.1221L2.47885 1.80665C1.81248 1.44317 1 1.92548 1 2.68454V11.3155C1 12.0745 1.81248 12.5568 2.47885 12.1934L10.3905 7.8779Z"
-        stroke={color}
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-      />
-    </svg>
+    {medium ? (
+      <svg
+        width="48"
+        height="48"
+        viewBox="0 0 48 48"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g clipPath="url(#clip0_1846_10419)">
+          <rect x="7.5" y="7.5" width="33" height="33" rx="6.5" stroke="#445AFF" strokeWidth="3" />
+          <path
+            d="M28.3905 24.8779C29.0854 24.4989 29.0854 23.5011 28.3905 23.1221L20.4789 18.8066C19.8125 18.4432 19 18.9255 19 19.6845V28.3155C19 29.0745 19.8125 29.5568 20.4789 29.1934L28.3905 24.8779Z"
+            stroke="#445AFF"
+            strokeWidth="3"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </g>
+        <defs>
+          <clipPath id="clip0_1846_10419">
+            <rect width="48" height="48" fill="white" />
+          </clipPath>
+        </defs>
+      </svg>
+    ) : (
+      <svg
+        width="12"
+        height="14"
+        viewBox="0 0 12 14"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M10.3905 7.8779C11.0854 7.49888 11.0854 6.50112 10.3905 6.1221L2.47885 1.80665C1.81248 1.44317 1 1.92548 1 2.68454V11.3155C1 12.0745 1.81248 12.5568 2.47885 12.1934L10.3905 7.8779Z"
+          stroke={color}
+          strokeWidth="2"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
+    )}
   </IconWrap>
 );
 
