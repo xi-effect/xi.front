4fa35564f374fe61460d6c25c8d7284e2ca1525f
unknownproperty <igor.buksheff@yandex.ru>
2022-10-31 19:13:11 +0300

    fix: fix css

--- kit/UserProfile/ContnetItems/Secure.tsx
+++ kit/UserProfile/ContnetItems/Secure.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Stack, Typography, Button } from '@mui/material';
+import { Stack, Typography, Button, useMediaQuery, Theme } from '@mui/material';
 
 import { inject, observer } from 'mobx-react';
 import { PasswordChangeDialog } from 'kit/PasswordChangeDialog';
@@ -10,143 +10,150 @@ const Secure = inject(
   'profileSt',
   'uiSt',
 )(
-  observer(({ profileSt, uiSt }) => (
-    <>
-      <Stack
-        direction="column"
-        justifyContent="flex-start"
-        alignItems="flex-start"
-        sx={{
-          bgcolor: 'grayscale.0',
-          width: '100%',
-          height: '140px',
-          borderRadius: '8px',
-          padding: '24px',
-          position: 'relative',
-        }}
-      >
-        <Typography
-          sx={{
-            fontWeight: 500,
-            fontSize: '24px',
-            lineHeight: '32px',
-          }}
-        >
-          Пароль
-        </Typography>
-        <Typography
-          sx={{
-            mt: '12px',
-            color: 'grayscale.40',
-            fontWeight: 400,
-            fontSize: '16px',
-            lineHeight: '24px',
-            width: '360px',
-          }}
-        >
-          Шифр, используемый для входа
-          <br />в аккаунт и подтверждения данных
-        </Typography>
-        <Button
-          onClick={() => uiSt.setDialogs('passwordChange', true)}
-          variant="contained"
-          sx={{
-            width: '160px',
-            height: '56px',
-            borderRadius: '8px',
-            position: 'absolute',
-            top: '24px',
-            right: '24px',
-            fontWeight: 500,
-            fontSize: '18px',
-            lineHeight: '22px',
-            textTransform: 'capitalize',
-            boxShadow: 0,
-            '&:hover': {
-              boxShadow: 0,
-            },
-          }}
-        >
-          Изменить
-        </Button>
-      </Stack>
+  observer(({ profileSt, uiSt }) => {
+    const mobile700: boolean = useMediaQuery((theme: Theme) => theme.breakpoints.down(1400));
 
-      <Stack
-        direction="column"
-        justifyContent="flex-start"
-        alignItems="flex-start"
-        sx={{
-          bgcolor: 'grayscale.0',
-          width: '100%',
-          height: '140px',
-          borderRadius: '8px',
-          padding: '24px',
-          position: 'relative',
-        }}
-      >
-        <Typography
+    return (
+      <>
+        <Stack
+          direction="column"
+          justifyContent="flex-start"
+          alignItems="flex-start"
           sx={{
-            fontWeight: 500,
-            fontSize: '24px',
-            lineHeight: '32px',
-          }}
-        >
-          Почта
-        </Typography>
-        <Typography
-          sx={{
-            mt: '12px',
-            color: 'grayscale.40',
-            fontWeight: 400,
-            fontSize: '16px',
-            lineHeight: '24px',
-            width: '360px',
-          }}
-        >
-          Электронный ящик
-          <br />
-          для восстановления доступа
-        </Typography>
-        <Typography
-          sx={{
-            color: 'grayscale.100',
-            fontWeight: 400,
-            fontSize: '16px',
-            lineHeight: '24px',
-            position: 'absolute',
-            top: '40px',
-            right: '200px',
-          }}
-        >
-          {profileSt.settings.email}
-        </Typography>
-        <Button
-          onClick={() => uiSt.setDialogs('emailChange', true)}
-          variant="contained"
-          sx={{
-            width: '160px',
-            height: '56px',
+            bgcolor: 'grayscale.0',
+            width: '100%',
             borderRadius: '8px',
-            position: 'absolute',
-            top: '24px',
-            right: '24px',
-            fontWeight: 500,
-            fontSize: '18px',
-            lineHeight: '22px',
-            textTransform: 'capitalize',
-            boxShadow: 0,
-            '&:hover': {
-              boxShadow: 0,
-            },
+            padding: '24px',
+            position: 'relative',
           }}
         >
-          Изменить
-        </Button>
-      </Stack>
-      <PasswordChangeDialog />
-      <EmailChangeDialog />
-    </>
-  )),
+          <Typography
+            sx={{
+              fontWeight: 500,
+              fontSize: '24px',
+              lineHeight: '32px',
+            }}
+          >
+            Пароль
+          </Typography>
+          <Typography
+            sx={{
+              mt: '12px',
+              color: 'grayscale.40',
+              fontWeight: 400,
+              fontSize: '16px',
+              lineHeight: '24px',
+              width: '360px',
+            }}
+          >
+            Шифр, используемый для входа
+            <br />в аккаунт и подтверждения данных
+          </Typography>
+          <Button
+            onClick={() => uiSt.setDialogs('passwordChange', true)}
+            variant="contained"
+            sx={{
+              width: '160px',
+              height: '56px',
+              borderRadius: '8px',
+              position: mobile700 ? '' : 'absolute',
+              top: mobile700 ? '20px' : '24px',
+              right: '24px',
+              fontWeight: 500,
+              fontSize: '18px',
+              lineHeight: '22px',
+              textTransform: 'capitalize',
+              ml: mobile700 ? '24px' : '',
+              mb: mobile700 ? '24px' : '',
+              boxShadow: 0,
+              '&:hover': {
+                boxShadow: 0,
+              },
+            }}
+          >
+            Изменить
+          </Button>
+        </Stack>
+
+        <Stack
+          direction="column"
+          justifyContent="flex-start"
+          alignItems="flex-start"
+          sx={{
+            bgcolor: 'grayscale.0',
+            width: '100%',
+            borderRadius: '8px',
+            padding: '24px',
+            position: 'relative',
+          }}
+        >
+          <Typography
+            sx={{
+              fontWeight: 500,
+              fontSize: '24px',
+              lineHeight: '32px',
+            }}
+          >
+            Почта
+          </Typography>
+          <Typography
+            sx={{
+              color: 'grayscale.100',
+              fontWeight: 400,
+              fontSize: '16px',
+              lineHeight: '24px',
+              position: mobile700 ? '' : 'absolute',
+              top: '40px',
+              right: '200px',
+              mt: mobile700 ? '12px' : '',
+            }}
+          >
+            {profileSt.settings.email}
+          </Typography>
+          <Typography
+            sx={{
+              mt: '12px',
+              color: 'grayscale.40',
+              fontWeight: 400,
+              fontSize: '16px',
+              lineHeight: '24px',
+              width: '360px',
+            }}
+          >
+            Электронный ящик
+            <br />
+            для восстановления доступа
+          </Typography>
+          <Button
+            onClick={() => uiSt.setDialogs('emailChange', true)}
+            variant="contained"
+            sx={{
+              width: '160px',
+              height: '56px',
+              borderRadius: '8px',
+              position: mobile700 ? '' : 'absolute',
+              top: '24px',
+              right: '24px',
+              fontWeight: 500,
+              fontSize: '18px',
+              lineHeight: '22px',
+              textTransform: 'capitalize',
+              boxShadow: 0,
+              ml: mobile700 ? '24px' : '',
+              mb: mobile700 ? '24px' : '',
+              '&:hover': {
+                boxShadow: 0,
+              },
+            }}
+          >
+            Изменить
+          </Button>
+        </Stack>
+        <PasswordChangeDialog />
+        <EmailChangeDialog />
+      </>
+    );
+  }),
 );
 
 export default Secure;
