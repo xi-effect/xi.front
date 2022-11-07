86dc480c2ee2f0d1c3c2c3cb1878514f49eb0e2d
EgorKirillov <80296253508@mail.ru>
2022-11-07 22:19:32 +0300

    fix: size logo, padding

--- pages/resetpassword/email.tsx
+++ pages/resetpassword/email.tsx
@@ -1,15 +1,13 @@
-import { useRouter } from 'next/router';
 import { Stack, Typography, useMediaQuery } from '@mui/material';
 import React from 'react';
 import { inject, observer } from 'mobx-react';
 import Form from 'components/ResetPassword/Email';
-import Image from 'next/image';
 import LayoutPages from 'kit/LayoutPages';
+import XiLogo from "../../kit/XiLogo";
 
 const PassResetEmail = inject('authorizationSt')(
   observer(({ authorizationSt }) => {
     const { passwordReset } = authorizationSt;
-    const router = useRouter();
 
     const isMobile = useMediaQuery('(max-width: 472px)');
 
@@ -25,12 +23,14 @@ const PassResetEmail = inject('authorizationSt')(
             direction="column"
             border={isMobile ? 'none' : '1px solid #E6E6E6'}
             borderRadius="16px"
-            padding={`${isMobile ? '65px' : '38px'} 32px 32px 32px`}
+            padding={`${isMobile ? '65px' : '32px'} 32px 32px 32px`}
             height="514px"
             width={isMobile ? '100%' : '420px'}
             spacing={2}
           >
-            <Image onClick={() => router.push('/')} src="/logo.svg" height={24} width={100} />
+            <Stack alignItems="center">
+              <XiLogo width="142px" height="24px"/>
+            </Stack>
             <Typography
               component="h1"
               variant="h5"
