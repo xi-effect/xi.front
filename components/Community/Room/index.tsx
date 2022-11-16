d28df3dd98dff92e6b8cd0d4b9dbc21a7465acb9
igor.b <igor.b@ati.su>
2022-11-17 00:38:11 +0300

    fix: socketTest remove

--- components/Community/Room/index.tsx
+++ components/Community/Room/index.tsx
@@ -2,83 +2,26 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 // @ts-nocheck
 import React from 'react';
-import useWebRTC, { LOCAL_VIDEO } from 'utils/useWebRTC';
 import { inject, observer } from 'mobx-react';
 import RootStore from 'store/rootStore';
-
-function layout(clientsNumber = 1) {
-  const pairs = Array.from({ length: clientsNumber }).reduce((acc, next, index, arr) => {
-    if (index % 2 === 0) {
-      acc.push(arr.slice(index, index + 2));
-    }
-
-    return acc;
-  }, []);
-
-  const rowsNumber = pairs.length;
-  const height = `${100 / rowsNumber}%`;
-
-  return pairs
-    .map((row, index, arr) => {
-      if (index === arr.length - 1 && row.length === 1) {
-        return [
-          {
-            width: '100%',
-            height,
-          },
-        ];
-      }
-
-      return row.map(() => ({
-        width: '50%',
-        height,
-      }));
-    })
-    .flat();
-}
 
 type RoomT = {
   rootStore: RootStore;
 };
 
 const Room: React.FC<RoomT> = inject('rootStore')(
-  observer((props) => {
-    const { rootStore }: RoomT = props;
-
-    const { clients, provideMediaRef } = useWebRTC(rootStore.socketTest, 1);
-
-    const videoLayout = layout(2);
-
-    console.log('clients', clients);
-    console.log('provideMediaRef', provideMediaRef);
-    console.log('videoLayout', videoLayout);
-
-    return (
-      <div
-        style={{
-          display: 'flex',
-          alignItems: 'center',
-          justifyContent: 'center',
-          flexWrap: 'wrap',
-        }}
-      >
-        {clients.map((clientID, index) => (
-          <div key={clientID} style={videoLayout[index]} id={clientID}>
-            <video
-              width="100%"
-              height="100%"
-              ref={(instance) => {
-                provideMediaRef(clientID, instance);
-              }}
-              autoPlay
-              playsInline
-              muted={clientID === LOCAL_VIDEO}
-            />
-          </div>
-        ))}
-      </div>
-    );
-  }),
+  observer(() => (
+    <div
+      style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        flexWrap: 'wrap',
+      }}
+    >
+      1
+    </div>
+  )),
 );
 
 export default Room;
