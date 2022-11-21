ddb2bc46a2253a05ecfd1e5f3223f421b1716d74
unknownproperty <igor.buksheff@yandex.ru>
2022-11-21 22:12:58 +0300

    fix(7711002): remove new injects

--- components/Community/Room/index.tsx
+++ components/Community/Room/index.tsx
@@ -2,26 +2,24 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 // @ts-nocheck
 import React from 'react';
-import { inject, observer } from 'mobx-react';
+import { observer } from 'mobx-react';
 import RootStore from 'store/rootStore';
 
 type RoomT = {
   rootStore: RootStore;
 };
 
-const Room: React.FC<RoomT> = inject('rootStore')(
-  observer(() => (
-    <div
-      style={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'center',
-        flexWrap: 'wrap',
-      }}
-    >
-      1
-    </div>
-  )),
-);
+const Room: React.FC<RoomT> = observer(() => (
+  <div
+    style={{
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      flexWrap: 'wrap',
+    }}
+  >
+    1
+  </div>
+));
 
 export default Room;
