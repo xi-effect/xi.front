3b801174d7739d871d5e110e45062d1a672c6fee
unknownproperty <igor.buksheff@yandex.ru>
2022-11-28 21:52:39 +0300

    fix: resetpassword page

--- pages/resetpassword/email.tsx
+++ pages/resetpassword/email.tsx
@@ -25,37 +25,27 @@ const PassResetEmail = observer(() => {
           direction="column"
           border={isMobile ? 'none' : '1px solid #E6E6E6'}
           borderRadius="16px"
-          padding={`${isMobile ? '65px' : '38px'} 32px 32px 32px`}
+          padding={`${isMobile ? '65px' : '32px'} 32px 32px 32px`}
           height="514px"
           width={isMobile ? '100%' : '420px'}
           spacing={2}
         >
-          <Stack
-            direction="column"
-            border={isMobile ? 'none' : '1px solid #E6E6E6'}
-            borderRadius="16px"
-            padding={`${isMobile ? '65px' : '32px'} 32px 32px 32px`}
-            height="514px"
-            width={isMobile ? '100%' : '420px'}
-            spacing={2}
-          >
-            <Stack alignItems="center">
-              <XiLogo width="142px" height="24px" />
-            </Stack>
-            <Typography
-              component="h1"
-              variant="h5"
-              textAlign="center"
-              sx={{
-                pb: '16px',
-                textAlign: 'center',
-                fontWeight: 600,
-              }}
-            >
-              {passwordReset.emailResetOk ? 'Письмо отправлено' : 'Восстановление'}
-            </Typography>
-            <Form />
+          <Stack alignItems="center">
+            <XiLogo width="142px" height="24px" />
           </Stack>
+          <Typography
+            component="h1"
+            variant="h5"
+            textAlign="center"
+            sx={{
+              pb: '16px',
+              textAlign: 'center',
+              fontWeight: 600,
+            }}
+          >
+            {passwordReset.emailResetOk ? 'Письмо отправлено' : 'Восстановление'}
+          </Typography>
+          <Form />
         </Stack>
       </Stack>
     </LayoutPages>
